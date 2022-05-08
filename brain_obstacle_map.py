76eecf6827b6724db97653c32310a81464a702c3
mahimaarora2208 <marora1@umd.edu>
2022-05-08 12:57:02 -0500

    added is coord inside the brain function

--- brain_obstacle_map.py
+++ brain_obstacle_map.py
@@ -14,6 +14,7 @@ Obstacle :
 # import math
 # import numpy as np
 import cv2
+import numpy as np
 
 
 IN_MARGIN = 1
@@ -22,6 +23,7 @@ RED = (0, 0, 255)
 GREEN = (0, 255, 0)
 BLUE = (255, 255, 0)
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 # Obstacle map colors
 ACCESSIBLE = (0, 0, 0)
 COMMON = (50, 50, 50)
@@ -36,8 +38,8 @@ SCALE_Y = 0.8
 START = (50, 50)
 GOAL = (200, 200)
 COST = None
-test_coord_x = 50
-test_coord_y = 50
+test_coord_x = 200
+test_coord_y = 240
 # This class is specific to Project 5
 
 
@@ -48,23 +50,21 @@ class BrainObstacleMap(object):
         self.start = start
         self.goal = goal
 
-    def detect_boundary_for_brain(self, image):
+    def detect_boundary_for_brain(self, image, bin_img):
         imgray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         ret, thresh = cv2.threshold(imgray, 0, 255, cv2.THRESH_BINARY)
         # cv2.imshow('thresh', thresh)
         # cv2.waitKey(0)
         contours, hierarchy = cv2.findContours(
             thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        cv2.drawContours(image, contours[1:5], -1, BLUE, 2)
-        # cv2.imshow('image', image)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
+        cv2.drawContours(bin_img, contours[1:5], -1, BLUE, 2)
+        return contours  # returns contours drawn
 
     def scale_img(self):
         # scales up the normal image input
         scaleUpImg_input = cv2.resize(self.img, None, fx=SCALE_X*5,
                                       fy=SCALE_Y*5, interpolation=cv2.INTER_LINEAR)
-       
+
         # cv2.imshow("Scaled Up Input Image", scaleUpImg_input)
 
         ret, bin_img = cv2.threshold(self.img, 100, 255, cv2.THRESH_BINARY)
@@ -153,6 +153,29 @@ class BrainObstacleMap(object):
         # print('x and y coordinates for point are: ', (x, y))
         return COST
 
+    def is_coordinate_inside_brain(self, contours, x, y):
+        # Make new brain image with filled contours
+        # white image
+        white_img = np.zeros([828, 828, 3], dtype=np.uint8)
+        white_img.fill(255)
+        stencil = np.zeros(white_img.shape).astype(white_img.dtype)
+        color = BLUE
+        cv2.fillPoly(stencil, contours[0:4], color)
+        result = cv2.bitwise_and(white_img, stencil)
+        # cv2.imshow("result_final", result)
+        # cv2.waitKey(0)
+
+        # check if coordinate is inside the brain or outside
+        if tuple(result[x, y]) == BLUE:
+            print('Point inside Brain...!')
+            return True
+        elif tuple(result[x, y]) == BLACK:
+            print('Point is outside the Brain...!')
+            return False
+        else:
+            print('Point is not able to identify its location...!')
+            return None
+
 
 if __name__ == "__main__":
     x_s, y_s = START
@@ -160,20 +183,27 @@ if __name__ == "__main__":
     input_img = cv2.imread('data/brain.png')
     map = BrainObstacleMap(input_img, START, GOAL)
     scaled_img = map.scale_img()
-    map.detect_boundary_for_brain(scaled_img[0])
+    contours_drawn = map.detect_boundary_for_brain(
+        scaled_img[0], scaled_img[1])
+    print('LOCATION :')    
+    map.is_coordinate_inside_brain(contours_drawn, test_coord_x, test_coord_y)
     scaled_img[0][x_s, y_s] = GREEN  # START SET input img
     scaled_img[0][x_g, y_g] = RED  # GOAL SET
     scaled_img[1][x_s, y_s] = GREEN  # START SET binary img
     scaled_img[1][x_g, y_g] = RED  # GOAL SET
-    cv2.imshow("Scaled Up Input Image", scaled_img[0])
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    cv2.imshow("Scaled Up Input Image", scaled_img[1])
+   
+    print('STATUS :')
     result = map.is_coordinate_occupied(
         scaled_img[1], test_coord_x, test_coord_y)
     print("Is coordinate occupied?")
     print(result)
     cost = map.get_cost_for_coordinate(
         scaled_img[0], test_coord_x, test_coord_y)
+    
+    print('VALUE :')    
     print("Cost of the coordinate?")
     print(cost)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     
